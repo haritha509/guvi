4481c90062658b0dbfcde4f368bcbb8b229c7923
haritha509 <41730838+haritha509@users.noreply.github.com>
2018-07-27 12:38:32 +0530

    Create 12.java

---
+++ 12.java
@@ -0,0 +1,24 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int b,e,i,r=1;
+		Scanner Sc=new Scanner(System.in);
+		b=Sc.nextInt();
+		e=Sc.nextInt();
+		for(i=0;i<e;i++)
+		{
+		r=r*b;	
+		}
+		System.out.println(r);
+		
+	}
+}
